d96d8b36f3e34c02340184fde025c05791399204
GMM1 <gmarx12@gmail.com>
2018-03-25 22:42:18 -0700

    Adding fourth line while on local machine to master

--- f2.c
+++ f2.c
@@ -1,3 +1,4 @@
 // file f2.c initial on local machine
 // another line while in master branch on local machine
 // Adding line 3 while on master branch on local machine
+// And a fourth line on local machine on master
